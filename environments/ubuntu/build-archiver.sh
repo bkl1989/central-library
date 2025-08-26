2721029126b2ca9825d786b7e9aa312620083f40
Ben <bkl1989@gmail.com>
2025-08-26 00:40:52 -0500

    this is a minimal example of what isn't working. why?

--- environments/ubuntu/build-archiver.sh
+++ environments/ubuntu/build-archiver.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export ARCHIVER_ROOT="$(./get-config.sh projectRoot)/services/archiver"
 rm $ARCHIVER_ROOT/archiver &> /dev/null
-g++-13 -std=c++23 $ARCHIVER_ROOT/archiver.cpp -o $ARCHIVER_ROOT/archiver
+g++-13 -std=c++23 $ARCHIVER_ROOT/archiver.cpp $ARCHIVER_ROOT/parser.cpp -o $ARCHIVER_ROOT/archiver
 
